9a428eb0039e5af019e0aa41c30262de336c749e
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-06-30 21:55:54 +1000

    added a functionality to update an existing team

--- src/components/DeleteForm.vue
+++ src/components/DeleteForm.vue
@@ -1,49 +1,47 @@
 <template>
-  <h2>Please confirm to delete the team.</h2>
-  <div class="buttons">
-    <Button label="Confirm" @click="deleteTeam" type="delete" />
-    <Button label="Cancel" @click="handleCancel" type="secondary" />
-  </div>
+	<h2>Please confirm to delete the team.</h2>
+	<div class="buttons">
+		<Button label="Confirm" @click="deleteTeam" type="delete" />
+		<Button label="Cancel" @click="handleCancel" type="secondary" />
+	</div>
 </template>
-  
-  <script>
+
+<script>
 import Button from "./Button.vue";
 import { db, storage } from "../firebase";
 import { deleteDoc, doc } from "firebase/firestore";
 import snackbar from "snackbar";
 import { ref, deleteObject } from "firebase/storage";
 export default {
-  components: { Button },
-  props: ["deleteDoc"],
-  emits: ["cancel", "confirm"],
-  methods: {
-    handleCancel() {
-      this.$emit("cancel");
-    },
-    async deleteTeam() {
-      const storageRef = ref(storage, this.deleteDoc);
-      await deleteObject(storageRef);
-      const docRef = doc(db, "teams", this.deleteDoc);
-      this.$emit("confirm");
-      await deleteDoc(docRef).then(() => {
-        snackbar.show("The team has been deleted successfully!");
-      });
-    },
-  },
+	components: { Button },
+	props: ["deleteDoc"],
+	emits: ["cancel", "confirm"],
+	methods: {
+		handleCancel() {
+			this.$emit("cancel");
+		},
+		async deleteTeam() {
+			const storageRef = ref(storage, this.deleteDoc);
+			await deleteObject(storageRef);
+			const docRef = doc(db, "teams", this.deleteDoc);
+			this.$emit("confirm");
+			await deleteDoc(docRef).then(() => {
+				snackbar.show("The team has been deleted successfully!");
+			});
+		},
+	},
 };
 </script>
-  
-  <style scoped>
+
+<style scoped>
 h2 {
-  color: black;
-  text-align: center;
-  text-transform: uppercase;
-  margin: 0 0 30px 0;
+	color: black;
+	text-align: center;
+	margin: 0 0 30px 0;
 }
 .buttons {
-  display: flex;
-  justify-content: center;
-  gap: 20px;
+	display: flex;
+	justify-content: center;
+	gap: 20px;
 }
 </style>
-  
--- src/components/UpdateForm.vue
+++ src/components/UpdateForm.vue
@@ -1,152 +1,141 @@
 <template>
-  <h2>Update the team details</h2>
-  <form @submit.prevent="updateTeam" ref="updateForm">
-    <div class="input-field">
-      <label for="name">Name:</label>
-      <input
-        v-model="name"
-        id="name"
-        type="text"
-        required
-        placeholder="e.g. Real Madrid"
-      />
-    </div>
-    <div class="input-field">
-      <label for="winningYears">Winning Years:</label>
-      <input
-        v-model="winningYears"
-        id="winningYears"
-        type="text"
-        pattern="\d{4}(,\s\d{4})*"
-        title="e.g. 2022, 2023"
-        required
-        placeholder="e.g. 2022, 2023"
-      />
-    </div>
-    <div class="input-field-logo">
-      <div>
-        <label for="logo">Logo:</label>
-        <input ref="inputFile" type="file" accept=".webp" id="logo" required />
-      </div>
-      <p class="warning-text" v-if="showFileSizeWarning">
-        Please select an image file smaller than 50KB.
-      </p>
-    </div>
-    <Button label="update" type="update" />
-    <i class="fa-solid fa-xmark" @click="handleClose"></i>
-  </form>
+	<h2>Update the team details</h2>
+	<form @submit.prevent="updateTeam" ref="updateForm">
+		<div class="input-field">
+			<label for="name">Name:</label>
+			<input
+				v-model="currentName"
+				id="name"
+				type="text"
+				required
+				placeholder="e.g. Real Madrid"
+			/>
+		</div>
+		<div class="input-field">
+			<label for="winningYears">Winning Years:</label>
+			<input
+				v-model="currentWinningYears"
+				id="winningYears"
+				type="text"
+				pattern="\d{4}(,\d{4})*"
+				title="e.g. 2022,2023"
+				required
+				placeholder="e.g. 2022,2023"
+			/>
+		</div>
+		<Button label="update" type="update" />
+		<i class="fa-solid fa-xmark" @click="handleClose"></i>
+	</form>
 </template>
-  
-  <script>
+
+<script>
 import Button from "./Button.vue";
-import { db, storage } from "../firebase";
+import { db } from "../firebase";
 import { updateDoc, doc } from "firebase/firestore";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export default {
-  components: { Button },
-  props: ["updateDoc"],
-  emits: ["update", "close"],
-  data() {
-    return {
-      id: null,
-      name: null,
-      winningYears: null,
-      showFileSizeWarning: false,
-    };
-  },
-  methods: {
-    async updateTeam() {
-      const file = this.$refs.inputFile.files[0];
-      if (file.size > 50000) {
-        this.showFileSizeWarning = true;
-        return;
-      } else {
-        this.showFileSizeWarning = false;
-        this.id = this.updateDoc;
-        this.name = this.name.trim();
-        this.winningYears = this.winningYears.split(", ");
-        this.winningYears = this.winningYears.map((year) =>
-          Number.parseInt(year)
-        );
+	components: { Button },
+	props: ["updateDoc"],
+	emits: ["update", "close"],
+	data() {
+		return {
+			name: "",
+			winningYears: "",
+		};
+	},
+	computed: {
+		currentName: {
+			get() {
+				return this.updateDoc?.name ?? "";
+			},
+			set(value) {
+				this.updateDoc.name = value;
+			},
+		},
+		currentWinningYears: {
+			get() {
+				return this.updateDoc?.winningYears ?? "";
+			},
+			set(value) {
+				this.updateDoc.winningYears = value;
+			},
+		},
+	},
+	methods: {
+		async updateTeam() {
+			this.name = this.currentName.trim();
+			this.winningYears = this.currentWinningYears.toString().split(",");
+			this.winningYears = this.winningYears.map((year) =>
+				Number.parseInt(year)
+			);
 
-        // Create a storage reference with a unique filename
-        const storageRef = ref(storage, this.id);
-        // Upload the file to Firebase Storage
-        await uploadBytes(storageRef, file);
-        // Get the public download URL of the uploaded file
-        const url = await getDownloadURL(storageRef);
-
-        const docRef = doc(db, "teams", this.updateDoc);
-        await updateDoc(docRef, {
-          logo: url,
-          name: this.name,
-          winningYears: this.winningYears,
-        });
-        this.name = "";
-        this.winningYears = "";
-        this.$refs.updateForm.reset();
-        this.$emit("update");
-      }
-    },
-    handleClose() {
-      this.$emit("close");
-    },
-  },
+			const docRef = doc(db, "teams", this.updateDoc.id);
+			await updateDoc(docRef, {
+				name: this.name,
+				winningYears: this.winningYears,
+			});
+			this.name = "";
+			this.winningYears = "";
+			this.$refs.updateForm.reset();
+			this.$emit("update");
+		},
+		handleClose() {
+			this.$emit("close");
+		},
+	},
 };
 </script>
-  
-  <style scoped>
+
+<style scoped>
 h2 {
-  color: black;
-  text-align: center;
-  text-transform: uppercase;
-  margin: 0 0 30px 0;
+	color: black;
+	text-align: center;
+	text-transform: uppercase;
+	margin: 0 0 30px 0;
 }
 .input-field {
-  margin-bottom: 30px;
+	margin-bottom: 30px;
 }
 .input-field-logo {
-  display: flex;
-  flex-direction: column;
-  margin: 40px 0;
+	display: flex;
+	flex-direction: column;
+	margin: 40px 0;
 }
 label {
-  padding: 0 5px;
-  color: #505050;
-  display: inline;
+	padding: 0 5px;
+	color: #505050;
+	display: inline;
 }
 input#name,
 input#winningYears {
-  color: #505050;
-  box-sizing: border-box;
-  padding: 10px;
-  outline: 0;
-  width: 100%;
-  border: 0;
-  background: transparent;
-  border-bottom: 1px solid #505050;
+	color: #505050;
+	box-sizing: border-box;
+	padding: 10px;
+	outline: 0;
+	width: 100%;
+	border: 0;
+	background: transparent;
+	border-bottom: 1px solid #505050;
 }
 input#name:focus,
 input#winningYears:focus {
-  border-bottom: 2px solid black;
+	border-bottom: 2px solid black;
 }
 Button {
-  width: 100%;
+	width: 100%;
 }
 i {
-  position: absolute;
-  top: 15px;
-  right: 15px;
-  font-size: 1.5rem;
-  cursor: pointer;
-  color: red;
+	position: absolute;
+	top: 15px;
+	right: 15px;
+	font-size: 1.5rem;
+	cursor: pointer;
+	color: red;
 }
 .warning-text {
-  font-size: 0.8rem;
-  color: red;
-  margin: 15px 0 0 0;
-  padding-left: 5px;
+	font-size: 0.8rem;
+	color: red;
+	margin: 15px 0 0 0;
+	padding-left: 5px;
 }
 </style>
-  
